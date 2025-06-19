205bc5c848aa55baa59724829013dfee4c4b3461
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-06-19 22:20:53 +0000

    feat: Create pin.ts zod validation schemas

---
+++ src/lib/validation/pin.ts
@@ -0,0 +1,32 @@
+import z from "zod";
+
+export const pinCreateSchema = z.object({
+  title: z.string().min(1).max(100),
+  url: z
+    .string()
+    .url()
+    .refine(
+      (url) =>
+        !!url.match(
+          /(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?/,
+        ),
+      "Invalid YouTube video URL",
+    ),
+  latitude: z.number().min(-90).max(90),
+  longitude: z.number().min(-180).max(180),
+});
+
+export type PinCreate = z.infer<typeof pinCreateSchema>;
+
+export const pinResponseSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  url: z.string().url(),
+  latitude: z.number(),
+  longitude: z.number(),
+  userId: z.string(),
+  userName: z.string(),
+  createdAt: z.number(),
+});
+
+export type PinResponse = z.infer<typeof pinResponseSchema>;
