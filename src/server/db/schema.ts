8303b0067aa8f2ad863bc208b135e51571ad505a
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-06-19 22:21:05 +0000

    feat: Define pins table schema with custom numeric type and ID generation

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,27 +1,42 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
+import IdPrefix, { generateId } from "@/lib/ids";
 import { sql } from "drizzle-orm";
-import { index, pgTableCreator } from "drizzle-orm/pg-core";
+import {
+  customType,
+  integer,
+  pgTable,
+  text,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `-_${name}`);
+const numericAsNumber = customType<{
+  data: number;
+  driverData: string;
+  config: { precision?: number; scale?: number };
+}>({
+  dataType(config) {
+    const precision = config?.precision ?? 10;
+    const scale = config?.scale ?? 2;
+    return `numeric(${precision}, ${scale})`;
+  },
+  fromDriver(value) {
+    return parseFloat(value);
+  },
+  toDriver(value) {
+    return value.toString();
+  },
+});
 
-export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [index("name_idx").on(t.name)],
-);
+export const pins = pgTable("pins", {
+  id: varchar("id", { length: 256 })
+    .primaryKey()
+    .$default(() => generateId(IdPrefix.PIN)),
+  title: varchar("title", { length: 100 }).notNull(),
+  url: text("url"),
+  latitude: numericAsNumber("latitude", { precision: 6, scale: 4 }).notNull(),
+  longitude: numericAsNumber("longitude", { precision: 7, scale: 4 }).notNull(),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+  userName: varchar("user_name", { length: 256 }).notNull(),
+  createdAt: integer("created_at")
+    .default(sql`(EXTRACT(EPOCH FROM NOW()))`)
+    .notNull(),
+});
