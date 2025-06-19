95d8f8b6f17210fc45430196e417b0cca9e176dc
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-06-19 22:20:26 +0000

    feat: Add IdPrefix enum and generateId function for unique ID generation

---
+++ src/lib/ids.ts
@@ -0,0 +1,18 @@
+enum IdPrefix {
+  PIN = "pin_",
+}
+
+export default IdPrefix;
+
+export function generateId(prefix: IdPrefix, length = 32) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let randomId = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomId += characters[randomIndex];
+  }
+
+  return `${prefix}${randomId}`;
+}
